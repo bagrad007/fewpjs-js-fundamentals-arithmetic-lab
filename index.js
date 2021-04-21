db4deb612534a7b9245291f58ad8c3a01c140433
Luke Gill <bagrad007@gmail.com>
2021-04-21 13:57:15 -0400

    commit tree d4d6c6c6431b4015478ae33e68d14da809773456

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2
+
+
+let random = Math.floor(Math.random() * 1) + 1
+
+
+let num3 = 4
+
+let num4 = 11
+let mod = num3 % num4
+
+let max = 20
